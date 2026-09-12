6b2d062aa37354ad98e8fb61202a5a484e966349
Divyanshu Singh <divyanshuand6@gmail.com>
2026-09-12 19:47:32 +0530

    Initial backend upload

---
+++ config/aws-config.js
@@ -0,0 +1,11 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "eu-north-1"
+});
+
+const s3 = new AWS.S3();
+
+const S3_BUCKET = "dsd05bucket";
+
+module.exports = { s3, S3_BUCKET };
